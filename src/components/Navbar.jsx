fd567dd096bb82eca37bcf5ae73df8d38c2dc53e
Phirakan Khongphaet <peerakran03g@gmail.com>
2024-12-11 18:28:02 +0700

    Change Path logout menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,7 +107,7 @@ function Navbar() {
               </MenuItem>
               <MenuItem>
                 <a
-                  href="#"
+                  href=""
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Sign out
