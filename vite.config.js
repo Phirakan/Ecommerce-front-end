2ebd9a620f8b3d99653d9b18e8abf5d2fdb8a9c4
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-01-16 17:24:26 +0700

    Edit config
    config port for docker

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, 
+    strictPort: true,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3333',
